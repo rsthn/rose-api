36d9252e47c37ea9f21cc63dd61df8f1c31557cb
Jonathan Palencia <strikeone88@rsthn.com>
2021-11-16 14:25:23 -0600

    - Fixed template db schema files. - Added more API files for a head start. - Published: v1.0.4

--- @local/db-schema.sql
+++ @local/db-schema.sql
@@ -1,32 +1,34 @@
 /* ******************************************************************************************* */
-DROP TABLE IF EXISTS x_directives;
-DROP TABLE IF EXISTS x_sessions;
-DROP TABLE IF EXISTS x_user_privileges;
-DROP TABLE IF EXISTS x_privileges;
-DROP TABLE IF EXISTS x_users;
+DROP TABLE IF EXISTS ##devices;
+DROP TABLE IF EXISTS ##directives;
+DROP TABLE IF EXISTS ##sessions;
+DROP TABLE IF EXISTS ##tokens;
+DROP TABLE IF EXISTS ##user_privileges;
+DROP TABLE IF EXISTS ##privileges;
+DROP TABLE IF EXISTS ##users;
 
 
 /* ******************************************************************************************* */
-CREATE TABLE x_directives
+CREATE TABLE ##directives
 (
 	subject_id int unsigned not null default 0,
-	type varchar(128) not null, /* verification_code */
+	type varchar(128) not null,
 
 	modified datetime,
 
-	s_value varchar(256) default '',
+	s_value varchar(8192) default null,
 	i_value int default 0,
 	t_value text default null
 )
 ENGINE=InnoDB CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci AUTO_INCREMENT=1;
 
-ALTER TABLE x_directives ADD PRIMARY KEY pk (subject_id, type);
-ALTER TABLE x_directives ADD INDEX n_type (type);
-ALTER TABLE x_directives ADD INDEX n_s_value (s_value);
+ALTER TABLE ##directives ADD PRIMARY KEY pk (subject_id, type);
+ALTER TABLE ##directives ADD INDEX n_type (type);
+ALTER TABLE ##directives ADD INDEX n_s_value (s_value);
 
 
 /* ******************************************************************************************* */
-CREATE TABLE x_users
+CREATE TABLE ##users
 (
     user_id int unsigned primary key auto_increment,
     created datetime default null,
@@ -44,11 +46,11 @@ CREATE TABLE x_users
 )
 ENGINE=InnoDB CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci AUTO_INCREMENT=1;
 
-ALTER TABLE x_users ADD index n_username (username);
-ALTER TABLE x_users ADD index n_is_active (is_active);
+ALTER TABLE ##users ADD index n_username (username);
+ALTER TABLE ##users ADD index n_is_active (is_active);
 
 
-CREATE TABLE x_privileges
+CREATE TABLE ##privileges
 (
     privilege_id int unsigned primary key auto_increment,
     name varchar(128) not null unique key
@@ -56,7 +58,7 @@ CREATE TABLE x_privileges
 ENGINE=InnoDB CHARSET=utf8mb4 COLLATE=utf8mb4_bin AUTO_INCREMENT=1;
 
 
-CREATE TABLE x_user_privileges
+CREATE TABLE ##user_privileges
 (
     user_id int unsigned not null,
     privilege_id int unsigned not null,
@@ -64,12 +66,12 @@ CREATE TABLE x_user_privileges
 
     primary key (user_id, privilege_id),
 
-    constraint foreign key (user_id) references x_users (user_id) on delete cascade,
-    constraint foreign key (privilege_id) references x_privileges (privilege_id) on delete cascade
+    constraint foreign key (user_id) references ##users (user_id) on delete cascade,
+    constraint foreign key (privilege_id) references ##privileges (privilege_id) on delete cascade
 )
 ENGINE=InnoDB CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
 
-CREATE TABLE x_tokens
+CREATE TABLE ##tokens
 (
 	token varchar(128) primary key,
 	user_id int unsigned not null,
@@ -77,15 +79,15 @@ CREATE TABLE x_tokens
 	is_authorized tinyint not null default 1,
 	is_active tinyint not null default 1,
 
-	constraint foreign key (user_id) references x_users (user_id) on delete cascade
+	constraint foreign key (user_id) references ##users (user_id) on delete cascade
 )
 ENGINE=InnoDB CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci AUTO_INCREMENT=1;
 
-ALTER TABLE x_tokens ADD index n_is_active (is_active);
+ALTER TABLE ##tokens ADD index n_is_active (is_active);
 
 
 /* ******************************************************************************************* */
-CREATE TABLE x_sessions
+CREATE TABLE ##sessions
 (
 	session_id varchar(48) primary key unique not null,
 
@@ -95,23 +97,10 @@ CREATE TABLE x_sessions
 	device_id varchar(48) default null,
 
 	user_id int unsigned default null,
-	constraint foreign key (user_id) references users (user_id) on delete cascade,
+	constraint foreign key (user_id) references ##users (user_id) on delete cascade,
 
 	data varchar(8192) default null
 )
 ENGINE=InnoDB CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
 
-
-DROP PROCEDURE IF EXISTS x_session_cleanup;
-DELIMITER //
-CREATE PROCEDURE x_session_cleanup (timeout INT UNSIGNED)
-BEGIN
-	DELETE FROM x_sessions
-	WHERE TIMESTAMPDIFF(SECOND, last_activity, NOW()) >= timeout;
-END //
-DELIMITER ;
-
-DROP EVENT IF EXISTS x_session_cleanup_evt;
-CREATE EVENT x_session_cleanup_evt
-ON SCHEDULE EVERY 1 DAY
-DO CALL x_session_cleanup (86400);
+ALTER TABLE ##sessions ADD index idx1 (device_id);
