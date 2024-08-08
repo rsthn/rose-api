64a99afe0d9fb807b832740cc68b02bd7eb44951
Jonathan Palencia <strikeone88@rsthn.com>
2024-08-08 12:05:53 -0600

    v2.0.15

--- periodic.php
+++ periodic.php
@@ -4,4 +4,9 @@ chdir(__DIR__);
 require_once ('vendor/autoload.php');
 
 Rose\Main::cli (dirname(__FILE__), true);
-Rose\Ext\Wind::run('lib/periodic.fn');
+try {
+    Rose\Ext\Wind::run('wind/periodic.fn');
+}
+catch (\Exception $e) {
+    \Rose\trace('[ERROR] [' . (new \Rose\DateTime()) . '] periodic: ' . $e->getMessage());
+}
